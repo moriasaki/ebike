3bf906b627f4396426173afd9a83ce07d9c6488e
moriasaki <2955370950@qq.com>
2026-03-13 15:36:40 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ERyder bike1 = new ERyder(
@@ -17,7 +21,15 @@ public class Main {
             "114-514-1919-810"
         );
         bike1.printRideDetails(15); 
-        bike2.printRideDetails(30); 
-        System.out.println("\nNote: calculateFare() is private - cannot call directly!");
+        bike2.printRideDetails(30);
+        String sent1 = "I was very satisfied with the service.";
+        String sent2 = "The e-Bike is quite comfortable to ride.";
+        String sent3 = "The battery life of the e-Bike is impressive.";
+        String sent4 = "The customer support was helpful and responsive.";
+        String sent5 = "I would recommend this e-Bike to my friends and family.";
+        Feedback userFeedback = new Feedback("wozhen", "fule", "antabaka@9999.com");
+        userFeedback.analyseFeedback(false, sent1, sent2, sent3, sent4, sent5);
+        System.out.println("\n" + userFeedback);
     }
 }
+
